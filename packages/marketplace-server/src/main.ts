1ce92ef9954ea1002685bcedaa1c8e6a2b6d6fb4
Nabhag Motivaras <65061890+Nabhag8848@users.noreply.github.com>
2025-06-25 15:30:09 +0530

    nestjs config

--- packages/marketplace-server/src/main.ts
+++ packages/marketplace-server/src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const config = app.get(ConfigService);
+
   const globalPrefix = 'v1';
   app.setGlobalPrefix(globalPrefix);
 
-  const port = process.env.SERVER_PORT || 3000;
+  const port = config.get<number>('SERVER_PORT', 3000);
 
   await app.listen(port);
 }
