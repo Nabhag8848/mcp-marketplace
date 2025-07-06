627bb569c78c964ace6bbc59b17da28fcc83b923
Nabhag Motivaras <65061890+Nabhag8848@users.noreply.github.com>
2025-07-06 19:31:41 +0530

    graphql setup for frontend with codegen

--- packages/marketplace-front/src/modules/app/components/app-router-providers.tsx
+++ packages/marketplace-front/src/modules/app/components/app-router-providers.tsx
@@ -2,14 +2,17 @@ import { Outlet, useLocation } from 'react-router-dom';
 import { StrictMode } from 'react';
 import { PageTitle } from '@/ui/utilities/page-title/components/page-title';
 import { getPageTitleFromPath } from '@/ui/utilities/page-title/utils/get-page-title-from-path';
+import { ApolloGraphQLProvider } from '@/graphql';
 
 export const AppRouterProviders = () => {
   const { pathname } = useLocation();
   const pageTitle = getPageTitleFromPath(pathname);
   return (
     <StrictMode>
-      <PageTitle title={pageTitle} />
-      <Outlet />
+      <ApolloGraphQLProvider>
+        <PageTitle title={pageTitle} />
+        <Outlet />
+      </ApolloGraphQLProvider>
     </StrictMode>
   );
 };
