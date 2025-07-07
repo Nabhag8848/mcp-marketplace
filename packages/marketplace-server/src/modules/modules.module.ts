d0b797adc681eb5c4242c44d7f9ea16ce25b77bb
Nabhag Motivaras <65061890+Nabhag8848@users.noreply.github.com>
2025-07-08 01:31:15 +0530

    graphql github

--- packages/marketplace-server/src/modules/modules.module.ts
+++ packages/marketplace-server/src/modules/modules.module.ts
@@ -3,8 +3,15 @@ import { QueueModule } from './queue/queue.module';
 import { GraphQLModule } from './graphql/graphql.module';
 import { ExampleModule } from './example/example.module';
 import { HealthModule } from './health/health.module';
+import { IntegrationModule } from './integration/integration.module';
 
 @Module({
-  imports: [QueueModule, GraphQLModule, ExampleModule, HealthModule],
+  imports: [
+    QueueModule,
+    GraphQLModule,
+    ExampleModule,
+    HealthModule,
+    IntegrationModule,
+  ],
 })
 export class ModulesModule {}
