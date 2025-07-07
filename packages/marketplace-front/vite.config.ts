4797511f5a7f134981d4e2ff3de8c428219fa689
Nabhag Motivaras <65061890+Nabhag8848@users.noreply.github.com>
2025-07-07 13:33:01 +0530

    fix: alias paths

--- packages/marketplace-front/vite.config.ts
+++ packages/marketplace-front/vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig(() => ({
     alias: {
       '@': path.resolve(__dirname, './src/modules'),
       '~': path.resolve(__dirname, './src'),
+      '@ui': path.resolve(__dirname, '../marketplace-ui/src'),
     },
   },
   build: {
