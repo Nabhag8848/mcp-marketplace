080afe0b3138beff3bf89720e6ebfff4e64da0ff
Nabhag Motivaras <65061890+Nabhag8848@users.noreply.github.com>
2025-08-21 13:09:01 +0530

    fix

--- packages/marketplace-server/src/database/redis/redis.config.ts
+++ packages/marketplace-server/src/database/redis/redis.config.ts
@@ -9,6 +9,8 @@ export const createRedisConfig = (
     host: configService.get<string>('REDIS_HOST', '127.0.0.1'),
     port: configService.get<number>('REDIS_PORT', 6378),
     db: configService.get<number>('REDIS_DB', 0),
+    username: configService.get<string>('REDIS_USERNAME', ''),
+    password: configService.get<string>('REDIS_PASSWORD', ''),
     lazyConnect: true,
     tls:
       isEqual(
